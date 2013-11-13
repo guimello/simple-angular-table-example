fa9873326a46aef4d24828c529777c2cbf47af73
guimello <guilhermesilvamello@gmail.com>
2013-11-13 21:25:50 -0200

    Adding the username filter

--- angular-simple-table.js
+++ angular-simple-table.js
@@ -10,19 +10,19 @@
    * @property {Array} users data
    * @property {Array} filtered users data
    * @property {Array} moods that are available
-   * @property {Object} mood that is selected in the filter
+   * @property {Object} mood that was filtered
    * @property {String} name that was filtered
    * @property {String} age that was filtered
+   * @property {String} username that was filtered
    */
-  app.controller('SimpleTable', ["$scope", function($scope) {
+  app.controller("SimpleTable", ["$scope", function($scope) {
     $scope.users = [
-      { "name": "Guilherme da Silva Mello", "age": 28, "mood": ":)" },
-      { "name": "Gordon Freeman", "age": 40, "mood": ":/" },
-      { "name": "Rei do camarote", "age": 35, "mood": ":(" },
-      { "name": "Bruce Dickinson", "age": 50, "mood": ":)" }
+      { "name": "Guilherme da Silva Mello", "age": 28, "username": "guimello", "mood": ":)" },
+      { "name": "Gordon Freeman", "age": 40, "username": "freeman",  "mood": ":/" },
+      { "name": "Rei do camarote", "age": 35, "username": "frei",  "mood": ":(" },
+      { "name": "Bruce Dickinson", "age": 50, "username": "dick",  "mood": ":)" }
     ];
 
-    $scope.filteredUsers = [];
 
     $scope.availableMood = [
       { "emoticon": ":)" },
@@ -30,10 +30,12 @@
       { "emoticon": ":(" }
     ];
 
-    // Will be defined by the view. It's here just for documenting
-    $scope.selectedMood;
+    // Those will be defined by the view. It's here just for documenting
+    $scope.filteredUsers;
+    $scope.filteredMood;
     $scope.filteredName;
     $scope.filteredAge;
+    $scope.filteredUsername;
   }]);
 
   /**
@@ -50,6 +52,26 @@
           return user.mood === mood.emoticon;
         });
       }
+    }
+  }]);
+
+  /**
+   * Filters the username
+   *
+   * It's implemented here this way as a contrast to the others that are
+   * perform in the html.
+   *
+   * A way to avoid all those filters in the view is to use one filter there
+   * and here in js apply all filters in sequency.
+   *
+   * @filter usernameFilter
+   * @return {Array} filtered users by username
+   */
+  filters.filter("usernameFilter", ['$filter', function($filter) {
+    return function usernameFilter(users, username) {
+      if (!users || !username) return users;
+
+      return $filter('filter')(users, { username: username });
     }
   }]);
 })(angular);
