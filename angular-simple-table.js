538bd2e051371b525e5408c118a5316199ee3f31
guimello <guilhermesilvamello@gmail.com>
2013-11-13 20:23:18 -0200

    Some docs :)

--- angular-simple-table.js
+++ angular-simple-table.js
@@ -1,39 +1,56 @@
-'use strict';
-var app = angular.module('App', ['App.filters']);
+(function(angular) {
+  'use strict';
 
-app.controller('SimpleTable', ["$scope", function($scope) {
-  $scope.users = [
-    { "name": "Guilherme da Silva Mello", "age": 28, "mood": ":)" },
-    { "name": "Gordon Freeman", "age": 40, "mood": ":/" },
-    { "name": "Rei do camarote", "age": 35, "mood": ":(" },
-    { "name": "Bruce Dickinson", "age": 50, "mood": ":)" }
-  ];
+  var app = angular.module('App', ['App.filters']);
+  var filters = angular.module('App.filters', []);
 
-  $scope.filteredUsers = [];
+  /**
+   * Represents the simple table view model
+   * @controller SimpleTable
+   * @property {Array} users data
+   * @property {Array} filtered users data
+   * @property {Array} moods that are available
+   * @property {Object} mood that is selected in the filter
+   */
+  app.controller('SimpleTable', ["$scope", function($scope) {
+    $scope.users = [
+      { "name": "Guilherme da Silva Mello", "age": 28, "mood": ":)" },
+      { "name": "Gordon Freeman", "age": 40, "mood": ":/" },
+      { "name": "Rei do camarote", "age": 35, "mood": ":(" },
+      { "name": "Bruce Dickinson", "age": 50, "mood": ":)" }
+    ];
 
-  $scope.availableMood = [
-    { "emoticon": ":)" },
-    { "emoticon": ":/" },
-    { "emoticon": ":(" }
-  ];
+    $scope.filteredUsers = [];
 
-  $scope.selectedMood;
-}]);
+    $scope.availableMood = [
+      { "emoticon": ":)" },
+      { "emoticon": ":/" },
+      { "emoticon": ":(" }
+    ];
 
-angular.module('App.filters', []).filter('moodFilter', [function() {
-  return function(users, mood) {
-    if (!users || !mood) {
-      return users;
-    } else {
-      var filteredUsers = [];
+    $scope.selectedMood;
+  }]);
 
-      angular.forEach(users, function(user) {
-        if (angular.equals(user.mood, mood.emoticon)) {
-          filteredUsers.push(user);
-        }
-      });
+  /**
+   * Custom mood filter that selects the users by mood
+   * @filter moodFilter
+   * @return {Array} filtered users by mood
+   */
+  filters.filter('moodFilter', [function() {
+    return function(users, mood) {
+      if (!users || !mood) {
+        return users;
+      } else {
+        var filteredUsers = [];
 
-      return filteredUsers;
+        angular.forEach(users, function(user) {
+          if (angular.equals(user.mood, mood.emoticon)) {
+            filteredUsers.push(user);
+          }
+        });
+
+        return filteredUsers;
+      }
     }
-  }
-}]);
+  }]);
+})(angular);
